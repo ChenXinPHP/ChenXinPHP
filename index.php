19a2ead280f647008bfb725da267b71653d3fd69
chenxin <haodf@chenxindeMacBook-Pro.local>
2016-08-09 18:39:46 +0800

    Add index.php and index.css

---
+++ index.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<meta content="width=device-width,initial-scale=1.0,maximum-scale=1.0" name="viewport">
+	<title>ChenXinPHP</title>
+	<link rel="stylesheet" type="text/css" href="index.css" />
+</head>
+<body>
+	<div class="navbar">
+		<div class="navtitle">ChenXinPHP</div>
+		<div class="navright">
+			<div><a href="signin.php"></a></div>
+			<div><a href="signup.php"></a></div>
+		</div>
+	</div>
+</body>
+</html>
